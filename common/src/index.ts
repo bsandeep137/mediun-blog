ec196a442c6d62cce91bab21e63ce7ee39858d5c
Sandeep Bachu <bsandeep137@gmail.com>
2025-03-11 14:21:44 +0530

    update

---
+++ common/src/index.ts
@@ -0,0 +1,27 @@
+import z from "zod"
+ export const signUpInput = z.object({
+    email: z.string().email(),
+    password : z.string().min(6),
+    name : z.string().optional()
+ })
+
+ export const signInInput = z.object({
+    email: z.string().email(),
+    password : z.string().min(6)
+ })
+
+ export const createBlogInput = z.object({
+    title : z.string(),
+    description: z.string(),
+ })
+
+ export const updateBlogInput = z.object({
+    title: z.string(),
+    description : z.string(),
+    id : z.string()
+ })
+
+export type SignUpInput = z.infer<typeof signUpInput>
+export type SignInInput = z.infer<typeof signInInput>
+export type CreateBlogInput = z.infer<typeof createBlogInput>
+export type UpdateBlogInput = z.infer<typeof updateBlogInput>
